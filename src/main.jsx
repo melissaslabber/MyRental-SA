b9c44b47ebff40844f3ef4d451ca5db68927115d
melissaslabber <melissa.slabber@gmail.com>
2026-09-13 22:36:05 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './mobile-fix.css'
 import './features.css'
+import './modules.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
