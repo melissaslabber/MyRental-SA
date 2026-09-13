2a334f5fc8df0626c17069e7a37d68045d4f9e06
melissaslabber <melissa.slabber@gmail.com>
2026-09-13 22:57:25 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import './mobile-fix.css'
 import './features.css'
 import './modules.css'
 import './legal.css'
+import './support.css'
+import './deposit.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>
